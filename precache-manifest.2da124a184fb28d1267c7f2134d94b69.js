4bc91c7e05d359fa727b08d34304bcc3c4a7755d
Fahad Hossain <oneloopone@gmail.com>
2019-07-09 18:30:11 +0600

    Updates

---
+++ precache-manifest.2da124a184fb28d1267c7f2134d94b69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00db3378d88beef353af0aa7d1b9f7a2",
+    "url": "/github-starsql/index.html"
+  },
+  {
+    "revision": "7f9ce32e82fabfaf51f4",
+    "url": "/github-starsql/static/css/main.94ad8322.chunk.css"
+  },
+  {
+    "revision": "f304fc43b0ac5474f345",
+    "url": "/github-starsql/static/js/2.ba9e13c7.chunk.js"
+  },
+  {
+    "revision": "7f9ce32e82fabfaf51f4",
+    "url": "/github-starsql/static/js/main.663f332f.chunk.js"
+  },
+  {
+    "revision": "f85ba799b0858d97e442",
+    "url": "/github-starsql/static/js/runtime~main.efbd8fb5.js"
+  }
+]);
